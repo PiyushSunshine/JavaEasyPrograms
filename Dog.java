00902f2aa85a00db4107b6b5dab76ddde08fa40f
PiyushSunshine <achkumarpiyush12@gmail.com>
2020-12-11 12:04:06 +0530

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,6 +1,6 @@
 package com.HackerRankJava;
 
-public class Dog extends animal_12HackerRank {
+public class Dog extends Animal {
 
 
     public Dog(){
@@ -13,24 +13,4 @@ public class Dog extends animal_12HackerRank {
     public void run(){
         System.out.println("Dog is running");
     }
-  // create another class of cat.
 }
-
-
-// before printing we need to set up animal first
-// for that we need to super clas after
-    /*
-    public Dog(){
-        System.out.println("A Dog has been created");
-    }
-     */
-
-    /*
-    Note that we can do the customization of the above code but we are not following it
-    below eg. to do the customization.
-    int age can be put here as well instead of superclass.
-    public Dog(int age){
-        super(15);   // we can add constructors here
-        System.out.println("A Dog has been created");
-    }
-     */
